3e5a32ec9fd08839fb140344b4cdf4065c3d700a
cao_boi_dia_nguc <jubivu@icloud.com>
2023-10-11 20:48:25 -0400

    fix payment visualize

---
+++ MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Mortgage
+//
+//  Created by Jubi on 10/11/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
